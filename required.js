47b942d296e9f9f84c48080433effdae09869e1e
Christian van Drunen <christian.van.drunen@hva.nl>
2025-03-13 15:11:39 +0100

    required turn on and off

---
+++ required.js
@@ -0,0 +1,54 @@
+function RequiredOnnOff(inputIds, shouldRequire = false){
+    var chkYes = document.getElementById("Nee1b1");
+
+
+var test = document.getElementsByName("1b3");
+
+inputIds.forEach(id => {
+var inputElement = document.getElementsByName(id);
+if (inputElement){
+    if (shouldRequire){
+        inputElement.forEach(input => input.setAttribute("required", "true"));    }
+    else{
+        inputElement.forEach(input => input.removeAttribute("required"));
+    }
+}
+});
+}
+
+
+
+
+// function RequiredOnnOff(){
+//     var chkYes = document.getElementById("Nee1b1");
+
+
+// var test = document.getElementById("Ja1b2");
+// var test2 = document.getElementById("kIP");
+
+// if(chkYes.checked){
+//     test.removeAttribute("required");
+//     test2.removeAttribute("required");
+// }
+// else{
+//     test.setAttribute("required", true);
+//     test2.setAttribute("required", true);
+//     console.log(test, test2);
+// }
+// }
+
+// function RequiredOnnOff(){
+//     var chkYes = document.getElementById("Nee1b1");
+
+
+// var test = document.getElementsByName("1b3");
+
+
+// if(chkYes.checked){
+//     test.forEach(input => input.removeAttribute("required"));
+// }
+// else{
+//     test.forEach(input => input.setAttribute("required", "true"));
+//     console.log(test);
+// }
+// }
