1b7f0cf6b414c27d9e754c043b36974e361dbf57
Christian van Drunen <christian.van.drunen@hva.nl>
2025-03-19 14:10:43 +0100

    Can generate forms

--- javascrip.js
+++ javascrip.js
@@ -1,10 +1,29 @@
-function generateFieldsets() {
-    let count = document.getElementById("numSections").value;
+function generateFieldsets(button) {
+  let temp = document.getElementsByTagName("template")[0];
+  let clon = temp.content.cloneNode(true);
+  
+  let parent = button.closest("#BOB"); // Find the closest #BOB parent
 
-    for (let i = 1; i <= count; i++) {
-        let temp = document.getElementsByTagName("template")[0];
-      
-      let clon = temp.content.cloneNode(true);
+  if (parent) {
+    parent.style.display = "none"; // Hide only this instance of #BOB
+  }
+
+  // document.getElementById("BOB").style.display="none";
+
   document.body.appendChild(clon);
-    }
 }
+
+document.addEventListener("DOMContentLoaded", () =>{
+  document.getElementById("kip").style.display="none";
+});
+
+// function generateFieldsets() {
+//   let count = document.getElementById("numSections").value;
+
+//   for (let i = 1; i <= count; i++) {
+//       let temp = document.getElementsByTagName("template")[0];
+    
+//     let clon = temp.content.cloneNode(true);
+// document.body.appendChild(clon);
+//   }
+// }
