e315f3c18a8834d4db113d11e7bce7446092cb59
Christian van Drunen <christian.van.drunen@hva.nl>
2025-05-06 01:13:16 +0200

    delete button and a lot of bug fixes

--- Question3.js
+++ Question3.js
@@ -5,46 +5,80 @@ let verkrijgerCount = 1;
 function generateFieldsets() {
   let temp = document.getElementsByTagName("template")[0];
   let clon = temp.content.cloneNode(true);
+  let inputs = clon.querySelectorAll("input");
+  let legend = clon.querySelector("legend");
 
   // geïnspireerd door Quy
-  let legend = clon.querySelector("legend");
-  if (legend) {
-    verkrijgerCount++;
-    legend.textContent = `verkrijger ${verkrijgerCount}`;
-  }
+  verkrijgerCount++;
+  legend.textContent = `verkrijger ${verkrijgerCount}`;
+
+  inputs.forEach((input) => {
+    let originalName = input.getAttribute("name");
+    input.setAttribute("name", `${originalName}_${verkrijgerCount}`);
+  });
+
   //met chatGPT
   document.getElementById("spawn-questions").appendChild(clon);
 }
 
+function deleteLatestVerkrijger() {
+  const verkrijgers = document.querySelectorAll(".verkrijgers");
+  if (verkrijgers.length > 0) {
+    const last = verkrijgers[verkrijgers.length - 1];
+    last.remove();
+    verkrijgerCount--;
+  }
+}
+
+let inputNames = ["3b2", "3b3", "3b5", "3b6", "3b7"];
+function UpdateList() {
+  inputNames = ["3b2", "3b3", "3b5", "3b6", "3b7"];
+
+  document.querySelectorAll(".verkrijgers input").forEach((input) => {
+    if (!input.name.includes("3b4")) {
+      inputNames.push(input.name);
+    }
+  });
+
+  console.log(inputNames);
+}
+
+function TurnOffButton() {
+  document.getElementById("generate-button").style.display = "none";
+  document.getElementById("verwijder-button").style.display = "none";
+}
+function TurnOnButton() {
+  document.getElementById("generate-button").style.display = "block";
+  document.getElementById("verwijder-button").style.display = "block";
+}
+
 // geinspireerd bij Quy
-document.addEventListener("DOMContentLoaded", () =>{
-  document.getElementById("javascript-failed").style.display="none";
-  TurnOffButton()
+window.addEventListener("pageshow", () => {
+  if (document.getElementById("Ja3b1").checked) {
+    TurnOnButton();
+  } else {
+    TurnOffButton();
+  }
 });
 
-function TurnOffButton(){
-  document.getElementById("generate-button").style.display= "none";
-}
-function TurnOnButton(){
-  document.getElementById("generate-button").style.display= "block";
-}
-
 document.getElementById("Nee3b1").addEventListener("change", function () {
-  const inputNames = ["3b2", "3b3", "3b4", "3b5", "3b6"];
   RequiredOnnOff(inputNames, false);
-  
   TurnOffButton();
 });
 
 document.getElementById("Ja3b1").addEventListener("change", function () {
-  const inputNames = ["3b2", "3b3", "3b4", "3b5", "3b6"];
   RequiredOnnOff(inputNames, true);
-  
   TurnOnButton();
 });
 
-document.getElementById("generate-button").addEventListener("click", function () {
+document
+  .getElementById("generate-button").addEventListener("click", function () {
     generateFieldsets();
-    RequiredOnnOff(["3b2", "3b3", "3b4", "3b5", "3b6"], true);
+    UpdateList();
+    RequiredOnnOff(inputNames, true);
   });
 
+document
+  .getElementById("verwijder-button").addEventListener("click", function () {
+    deleteLatestVerkrijger();
+  });
